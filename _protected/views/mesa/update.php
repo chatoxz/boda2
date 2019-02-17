f0a4fc840b1e82f4b03b48e54eb48e29c04abe98
chatoxz <carrerasssebastian@gmail.com>
2019-02-16 21:08:17 -0300

    nombre mesa en update

--- _protected/views/mesa/update.php
+++ _protected/views/mesa/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Mesa */
 
-$this->title = 'Actualizar Mesa: ' . ' ' . $model->id;
+$this->title = 'Actualizar Mesa: ' . ' ' . $model->nombre;
 $this->params['breadcrumbs'][] = ['label' => 'Mesa', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id, 'id_boda' => $model->id_boda]];
 $this->params['breadcrumbs'][] = 'Update';
