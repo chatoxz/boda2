a94c76e389a1ff056c19178b744c2bab243e9c55
chatoxz <carrerasssebastian@gmail.com>
2019-02-16 21:48:21 -0300

    db

--- _protected/config/db.php
+++ _protected/config/db.php
@@ -3,9 +3,9 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=boda',
-     /*'username' => 'appe',
-    'password' => 'appesDB2018',*/
-   'username' => 'root',
-    'password' => '',
+     'username' => 'appe',
+    'password' => 'appesDB2018',
+   /*'username' => 'root',
+    'password' => '',*/
     'charset' => 'utf8',
 ];
