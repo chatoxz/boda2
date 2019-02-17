e36a621f46e44531599f0acdf43a6d735ee812c7
chatoxz <carrerasssebastian@gmail.com>
2019-02-16 21:07:02 -0300

    meas invita form fix

--- _protected/views/mesa-invitado/update.php
+++ _protected/views/mesa-invitado/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\MesaInvitado */
 
-$this->title = 'Actualizar Mesa Invitado: ' . ' ' . $model->nombre;
+$this->title = 'Actualizar Mesa del Invitado ';
 $this->params['breadcrumbs'][] = ['label' => 'Mesa Invitado', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
