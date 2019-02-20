50cfccf8b31320cc9ed67bd18e83728972d84b60
chatoxz <carrerasssebastian@gmail.com>
2019-02-20 20:14:02 -0300

    sacado db.php

--- _protected/views/user/_form.php
+++ _protected/views/user/_form.php
@@ -9,39 +9,40 @@ use yii\widgets\ActiveForm;
 /* @var $form yii\widgets\ActiveForm */
 ?>
 <div class="user-form">
-
     <?php $form = ActiveForm::begin(['id' => 'form-user']); ?>
 
-        <?= $form->field($user, 'username')->textInput(
-                ['placeholder' => Yii::t('app', 'Crear username'), 'autofocus' => true]) ?>
+    <?= $form->field($user, 'username')->textInput(
+        ['placeholder' => Yii::t('app', 'Crear username'), 'autofocus' => true]) ?>
 
-        <?= $form->field($user, 'email')->input('email', ['placeholder' => Yii::t('app', 'Enter e-mail')]) ?>
+    <?= $form->field($user, 'email')->input('email', ['placeholder' => Yii::t('app', 'Enter e-mail')]) ?>
 
-        <?php if ($user->scenario === 'create'): ?>
+    <?php if ($user->scenario === 'create'): ?>
 
-            <?= $form->field($user, 'password')->widget(PasswordInput::classname(),
-                ['options' => ['placeholder' => Yii::t('app', 'Crear password')]]) ?>
+        <?= $form->field($user, 'password')->widget(PasswordInput::classname(),
+            ['options' => ['placeholder' => Yii::t('app', 'Crear password')]]) ?>
 
-        <?php else: ?>
+    <?php else: ?>
 
-            <?= $form->field($user, 'password')->widget(PasswordInput::classname(),
-                     ['options' => ['placeholder' => Yii::t('app', 'Change password ( if you want )')]]) ?>
+        <?= $form->field($user, 'password')->widget(PasswordInput::classname(),
+            ['options' => ['placeholder' => Yii::t('app', 'Change password ( if you want )')]]) ?>
 
-        <?php endif ?>
+    <?php endif ?>
 
+    <?php
+    if($user->getRole()->all()[0]['item_name'] == 'theCreator'){ ?>
     <div class="row">
-    <div class="col-md-6">
+        <div class="col-md-6">
 
-        <?= $form->field($user, 'status')->dropDownList($user->statusList) ?>
+            <?= $form->field($user, 'status')->dropDownList($user->statusList) ?>
 
-        <?php foreach (AuthItem::getRoles() as $item_name): ?>
-            <?php $roles[$item_name->name] = $item_name->name ?>
-        <?php endforeach ?>
-        <?= $form->field($user, 'item_name')->dropDownList($roles) ?>
+            <?php foreach (AuthItem::getRoles() as $item_name): ?>
+                <?php $roles[$item_name->name] = $item_name->name ?>
+            <?php endforeach ?>
+            <?= $form->field($user, 'item_name')->dropDownList($roles) ?>
 
+        </div>
     </div>
-    </div>
-
+    <?php } ?>
     <div class="form-group">
         <?= Html::submitButton($user->isNewRecord ? Yii::t('app', 'Create')
             : Yii::t('app', 'Update'), ['class' => $user->isNewRecord
