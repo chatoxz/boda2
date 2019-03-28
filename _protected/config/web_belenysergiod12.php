b41b9deb6ec893e61d4817c9cf37c47eb35f62b2
chatoxz <carrerasssebastian@gmail.com>
2019-03-28 19:56:23 -0300

    sacado db.php

---
+++ _protected/config/web_belenysergiod12.php
@@ -0,0 +1,169 @@
+<?php
+
+$params = require(__DIR__ . '/params_belenysergio.php');
+
+$config = [
+    'id' => 'basic',
+    'name' => 'Belen y Sergio',
+    'language' => 'es',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log', 'app\components\Aliases'],
+    'modules' => [
+        'db-manager' => [
+            'class' => '\bs\dbManager\Module',
+            // path to directory for the dumps
+            'path' => '@app/backups',
+            // list of registerd db-components
+            'dbList' => ['db'],
+            'as access' => [
+                'class' => 'yii\filters\AccessControl',
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['admin'],
+                    ],
+                ],
+            ],
+        ],
+        'gridview' =>  [
+            'class' => '\kartik\grid\Module'
+        ],
+        'datecontrol' => [
+            'class' => '\kartik\datecontrol\Module',
+            // see settings on http://demos.krajee.com/datecontrol#module
+        ],
+        // If you use tree table
+        'treemanager' =>  [
+            'class' => '\kartik\tree\Module',
+            // see settings on http://demos.krajee.com/tree-manager#module
+        ]
+    ],
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'EX1qEGWqDHgDnbuc6bHTE28UIEHY5Bz6',
+        ],
+        // ESTA DESACTIVADO XQ DAN ERROR LOS GLYPH ICONS, PARA ACTIVARLO DE VUELTA DESCOMENTAR assetmanager QUE ESTA ABAJO
+        'view' => [
+
+            'theme' => [
+                'pathMap' => ['@app/views' => '@webroot/themes/light/views'],
+                'baseUrl' => '@web/themes/light',
+            ],
+        ],
+        'assetManager' => [
+            'class' => 'yii\web\AssetManager',
+            'bundles' => [
+                'yii\web\JqueryAsset' => [
+                    'js' => [
+                        YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                    ]
+                ],
+                'yii\bootstrap\BootstrapAsset' => [
+                    'css' => [
+                        YII_ENV_DEV ? 'css/bootstrap.css' : 'css/bootstrap.min.css',
+                    ]
+                ],
+                'yii\bootstrap\BootstrapPluginAsset' => [
+                    'js' => [
+                        YII_ENV_DEV ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
+                    ]
+                ]
+            ],
+        ],
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            'rules' => [
+                '<alias:\w+>' => 'site/<alias>',
+            ],
+        ],
+        'user' => [
+            'identityClass' => 'app\models\UserIdentity',
+            'enableAutoLogin' => true,
+        ],
+        'session' => [
+            'class' => 'yii\web\Session',
+            'savePath' => '@app/runtime/session'
+        ],
+        'authManager' => [
+            'class' => 'yii\rbac\DbManager',
+            'cache' => 'cache',
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@app/mail',
+            //'useFileTransport' => false, //set this property to false to send mails to real email addresses
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'emailsmandar',
+                //'username' => 'carrerasssebastian',
+                'password' => '17KakarotO17',
+                //'password' => '17chato84',
+                //'port' => '25',
+                'port' => '587',
+                'encryption' => 'tls',
+                'streamOptions'=>[
+                    'ssl'=>[
+                        'verify_peer'=>false,
+                        'verify_peer_name'=>false,
+                        'allow_self_signed'=>true
+                    ]
+                ]
+            ],
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'i18n' => [
+            'translations' => [
+                'app*' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => 'translations',
+                    'sourceLanguage' => 'en',
+                ],
+                'yii' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => 'translations',
+                    'sourceLanguage' => 'en'
+                ],
+                'mtrelt' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => 'translations',
+                ],
+                'rbac-admin' => [
+                    'class' => 'yii\i18n\PhpMessageSource',
+                    'basePath' => 'translations',
+                ],
+            ],
+        ],
+        'db' => require(__DIR__ . '/db.php'),
+    ],
+    'params' => $params,
+    'defaultRoute' => 'site/belenysergiod12',
+];
+
+if (YII_ENV_DEV) {
+    // configuration adjustments for 'dev' environment
+    $config['bootstrap'][] = 'debug';
+    $config['modules']['debug'] = ['class' => 'yii\debug\Module'];
+
+    $config['bootstrap'][] = 'gii';
+    $config['modules']['gii'] = ['class' => 'yii\gii\Module'];
+}
+
+return $config;
